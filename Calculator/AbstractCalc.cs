1739a1a2b7b60a491e393092027eea14576fe1f5
Bulykin Egor <egorbulykin@gmail.com>
2021-11-11 01:04:21 +0300

    1 добавил абстрактный класс

---
+++ Calculator/AbstractCalc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    abstract class AbstractCalc
+    {
+        string make;
+        public abstract string GetMake();
+        public abstract double Add();
+        public abstract double Div();
+        public abstract double Mul();
+        public abstract double Sub();
+    }
+}
