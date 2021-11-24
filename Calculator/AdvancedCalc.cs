cb3120aa29f8abc45f0f627434aa8f290ab5995b
gospodiskoro20@gmail.com <gospodiskoro20@gmail.com>
2021-11-24 21:32:46 +0200

    Work is done!

--- Calculator/AdvancedCalc.cs
+++ Calculator/AdvancedCalc.cs
@@ -6,9 +6,14 @@ namespace Calculator
 {
     class AdvancedCalc : OrdinaryCalc, IAdvanced
     {
-        public static double CalcCtg(int Degree)
+        public static double CalcCtg(int radDegree)
         {
-
+            double degree = radDegree * (Math.PI / 180);
+            if (degree < -Math.PI || degree > Math.PI)
+            {
+                throw new ArgumentException();
+            }
+            return 1 / Math.Tan(radDegree);
         }
     }
 }
